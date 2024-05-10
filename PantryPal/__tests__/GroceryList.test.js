ec0901c3eb38934bfff4ae79e9c7aa71d086239e
mdubiarco <mdubiarco@csuchico.edu>
2024-05-09 19:54:08 -0700

    grocerylist test

--- PantryPal/__tests__/GroceryList.test.js
+++ PantryPal/__tests__/GroceryList.test.js
@@ -15,3 +15,28 @@ test('adds an item to the list', () => {
 
   expect(getByText('Tomatoes : 2')).toBeTruthy();
 });
+
+test('clears the list', () => {
+  const { getByPlaceholderText, getByText } = render(<GroceryList />);
+
+  // Add items to the list
+  const foodItemInput = getByPlaceholderText('Enter a Food Item');
+  const quantityInput = getByPlaceholderText('Enter Quantity');
+  const addItemButton = getByText('Add Item');
+
+  fireEvent.changeText(foodItemInput, 'Apples');
+  fireEvent.changeText(quantityInput, '4');
+  fireEvent.press(addItemButton);
+
+  fireEvent.changeText(foodItemInput, 'Grapes');
+  fireEvent.changeText(quantityInput, '2');
+  fireEvent.press(addItemButton);
+
+  expect(getByText('Apples : 4')).toBeTruthy();
+  expect(getByText('Grapes : 2')).toBeTruthy();
+
+  // Clear the list
+  const clearButton = getByText('Clear List');
+  fireEvent.press(clearButton);
+});
+
